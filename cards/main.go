f8c607b275a2cc9b4ded1ea5d9fcebb6ac6dd921
Leone Perdigão <leone.perdigao@squadra.com.br>
2019-02-19 11:10:04 -0300

    Playing with functions

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "File of Diamonds"
 }
