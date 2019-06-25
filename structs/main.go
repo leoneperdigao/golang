e48442d548752c41473ad95a21a980f6b509fc3f
Leone Perdigão <leone.lage@gmail.com>
2019-06-25 09:35:51 -0300

    Adding inner structs

--- structs/main.go
+++ structs/main.go
@@ -5,12 +5,22 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
 	alex := person{
 		firstName: "Alex",
 		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
 	}
 	fmt.Println(alex)
 }
